09befe6525a9fc905ad5f8b056f4a1dd5b7cb3b6
Tanish-Sarkar <tanishnitch@gmail.com>
2025-11-24 11:11:54 +0530

    models

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    sttus = db.Column(db.string(20), default = "Pending")
+
